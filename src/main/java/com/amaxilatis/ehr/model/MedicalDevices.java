faef2c20a6086837925f27eb221bf2ee8cfbb5d1
Dimitris Zarras <zarras.dim@gmail.com>
2015-09-16 14:20:28 +0300

    Update MedicalDevices model class.

--- src/main/java/com/amaxilatis/ehr/model/MedicalDevices.java
+++ src/main/java/com/amaxilatis/ehr/model/MedicalDevices.java
@@ -1,14 +1,50 @@
 package com.amaxilatis.ehr.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 /**
  * @author Dimitrios Amaxilatis.
+ * @author <href="mailto:dzarras@cti.gr">Dimitris Zarras</a>
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class MedicalDevices {
+    private Integer medicalDevicesId;
+    private String codingId;
     private String brandName;
-    private int codingId;
+    private String code;
+    private String codeSystem;
+    private String description;
+
+    /**
+     * <p>Default no argument constructor. Initializes the fields required
+     * to add a new {@link MedicalDevices} to EHR to non null values.</p>
+     */
+    public MedicalDevices() {
+        medicalDevicesId = null;
+        codingId = "no_value";
+        brandName = "no_value";
+        code = null;
+        codeSystem = null;
+        description = "no_value";
+    }
+
+    public Integer getMedicalDevicesId() {
+        return medicalDevicesId;
+    }
+
+    public void setMedicalDevicesId(Integer medicalDevicesId) {
+        this.medicalDevicesId = medicalDevicesId;
+    }
+
+    public String getCodingId() {
+        return codingId;
+    }
+
+    public void setCodingId(String codingId) {
+        this.codingId = codingId;
+    }
 
     public String getBrandName() {
         return brandName;
@@ -18,11 +54,27 @@ public class MedicalDevices {
         this.brandName = brandName;
     }
 
-    public int getCodingId() {
-        return codingId;
+    public String getCode() {
+        return code;
     }
 
-    public void setCodingId(int codingId) {
-        this.codingId = codingId;
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCodeSystem() {
+        return codeSystem;
+    }
+
+    public void setCodeSystem(String codeSystem) {
+        this.codeSystem = codeSystem;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
