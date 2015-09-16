ae8567cce04a87eee7e3f602929a5f50ac83487a
Dimitris Zarras <zarras.dim@gmail.com>
2015-09-16 14:57:53 +0300

    Update Scheduling model.

--- src/main/java/com/amaxilatis/ehr/model/Scheduling.java
+++ src/main/java/com/amaxilatis/ehr/model/Scheduling.java
@@ -1,15 +1,50 @@
 package com.amaxilatis.ehr.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 /**
  * @author Dimitrios Amaxilatis.
+ * @author <href="mailto:dzarras@cti.gr">Dimitris Zarras</a>
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Scheduling {
-    private String physicianId;
-    private int durationMinutes;
+    private Integer schedulingId;
     private String patientId;
+    private String physicianId;
+    private String appointmentDate;
+    private String description;
+    private String durationMinutes;
+
+    /**
+     * <p>Default no-argument constructor that sets the required fields to insert a new {@link Scheduling}
+     * to EHR to non-null values.</p>
+     */
+    public Scheduling() {
+        schedulingId = null;
+        patientId = "no_value";
+        physicianId = "no_value";
+        appointmentDate = "no_value";
+        description = "no_value";
+        durationMinutes = "no_value";
+    }
+
+    public Integer getSchedulingId() {
+        return schedulingId;
+    }
+
+    public void setSchedulingId(Integer schedulingId) {
+        this.schedulingId = schedulingId;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
 
     public String getPhysicianId() {
         return physicianId;
@@ -19,19 +54,39 @@ public class Scheduling {
         this.physicianId = physicianId;
     }
 
-    public int getDurationMinutes() {
+    public String getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(String appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDurationMinutes() {
         return durationMinutes;
     }
 
-    public void setDurationMinutes(int durationMinutes) {
+    public void setDurationMinutes(String durationMinutes) {
         this.durationMinutes = durationMinutes;
     }
 
-    public String getPatientId() {
-        return patientId;
-    }
-
-    public void setPatientId(String patientId) {
-        this.patientId = patientId;
+    @Override
+    public String toString() {
+        return "Scheduling{" +
+                "schedulingId=" + schedulingId +
+                ", patientId='" + patientId + '\'' +
+                ", physicianId='" + physicianId + '\'' +
+                ", appointmentDate='" + appointmentDate + '\'' +
+                ", description='" + description + '\'' +
+                ", durationMinutes='" + durationMinutes + '\'' +
+                '}';
     }
 }
