1e4e1e567ff7740ef47c342922db3d7e3ec82d60
Dimitris Zarras <zarras.dim@gmail.com>
2015-09-16 14:24:45 +0300

    PatientIdExistsException extends Exception not Throwable.

--- src/main/java/com/amaxilatis/ehr/exception/PatientIdExistsException.java
+++ src/main/java/com/amaxilatis/ehr/exception/PatientIdExistsException.java
@@ -2,6 +2,8 @@ package com.amaxilatis.ehr.exception;
 
 /**
  * @author Dimitrios Amaxilatis.
+ * @author <href="mailto:dzarras@cti.gr">Dimitris Zarras</a>
  */
-public class PatientIdExistsException extends Throwable {
+public class PatientIdExistsException extends Exception {
+
 }
