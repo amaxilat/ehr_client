6d61236db3586c0c01d26ee6aa75662adfe851c8
Dimitris Zarras <zarras.dim@gmail.com>
2015-09-16 15:23:16 +0300

    Model for a List of Schedulings.

---
+++ src/main/java/com/amaxilatis/ehr/model/list/SchedulingList.java
@@ -0,0 +1,32 @@
+package com.amaxilatis.ehr.model.list;
+
+import com.amaxilatis.ehr.model.Scheduling;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+/**
+ * A {@link List} of {@link Scheduling}s.
+ *
+ * @author <href="mailto:dzarras@cti.gr">Dimitris Zarras</a>
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SchedulingList {
+
+    private List<Scheduling> scheduling;
+
+    public List<Scheduling> getScheduling() {
+        return scheduling;
+    }
+
+    public void setScheduling(List<Scheduling> scheduling) {
+        this.scheduling = scheduling;
+    }
+
+    @Override
+    public String toString() {
+        return "SchedulingList{" +
+                "scheduling=" + scheduling +
+                '}';
+    }
+}
