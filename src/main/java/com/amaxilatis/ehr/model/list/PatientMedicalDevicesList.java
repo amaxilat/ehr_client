0de5d0d16926f673a75dbea2316cc9cc833334d6
Dimitris Zarras <zarras.dim@gmail.com>
2015-09-17 13:25:30 +0300

    Adding PatientMedicalDevicesList.

---
+++ src/main/java/com/amaxilatis/ehr/model/list/PatientMedicalDevicesList.java
@@ -0,0 +1,32 @@
+package com.amaxilatis.ehr.model.list;
+
+import com.amaxilatis.ehr.model.PatientMedicalDevices;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+/**
+ * <p>A {@link List} of {@link PatientMedicalDevices}.</p>
+ *
+ * @author <href="mailto:dzarras@cti.gr">Dimitris Zarras</a>
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PatientMedicalDevicesList {
+
+    private List<PatientMedicalDevices> patientMedicalDevices;
+
+    public List<PatientMedicalDevices> getPatientMedicalDevices() {
+        return patientMedicalDevices;
+    }
+
+    public void setPatientMedicalDevices(List<PatientMedicalDevices> patientMedicalDevices) {
+        this.patientMedicalDevices = patientMedicalDevices;
+    }
+
+    @Override
+    public String toString() {
+        return "PatientMedicalDevicesList{" +
+                "patientMedicalDevices=" + patientMedicalDevices +
+                '}';
+    }
+}
