e60a0d8a2e1bd8469fd1662557616cc339ad9d4b
Dimitris Zarras <zarras.dim@gmail.com>
2015-09-17 12:12:16 +0300

    Adding LabAnalyisisList.

---
+++ src/main/java/com/amaxilatis/ehr/model/list/LabAnalysisList.java
@@ -0,0 +1,32 @@
+package com.amaxilatis.ehr.model.list;
+
+import com.amaxilatis.ehr.model.LabAnalysis;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+/**
+ * <p>A {@link List} of {@link LabAnalysis}.</p>
+ *
+ * @author <href="mailto:dzarras@cti.gr">Dimitris Zarras</a>
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class LabAnalysisList {
+
+    private List<LabAnalysis> labAnalysis;
+
+    public List<LabAnalysis> getLabAnalysis() {
+        return labAnalysis;
+    }
+
+    public void setLabAnalysis(List<LabAnalysis> labAnalysis) {
+        this.labAnalysis = labAnalysis;
+    }
+
+    @Override
+    public String toString() {
+        return "LabAnalysisList{" +
+                "labAnalysis=" + labAnalysis +
+                '}';
+    }
+}
