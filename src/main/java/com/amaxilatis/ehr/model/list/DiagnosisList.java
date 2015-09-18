14ad54af4f923970b02c2d09086c87b5a1bba763
Dimitris Zarras <zarras.dim@gmail.com>
2015-09-18 15:30:54 +0300

    Adding DiagnosisList.

---
+++ src/main/java/com/amaxilatis/ehr/model/list/DiagnosisList.java
@@ -0,0 +1,32 @@
+package com.amaxilatis.ehr.model.list;
+
+import com.amaxilatis.ehr.model.Diagnosis;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+/**
+ * <p>A {@link List} of {@link Diagnosis}.</p>
+ *
+ * @author <href="mailto:dzarras@cti.gr">Dimitris Zarras</a>
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class DiagnosisList {
+
+    private List<Diagnosis> diagnosis;
+
+    public List<Diagnosis> getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(List<Diagnosis> diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    @Override
+    public String toString() {
+        return "DiagnosisList{" +
+                "diagnosis=" + diagnosis +
+                '}';
+    }
+}
