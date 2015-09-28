a58753081f49ad1ae4c16baed4eba9c84f849dce
Dimitris Zarras <zarras.dim@gmail.com>
2015-09-28 12:36:52 +0300

    Update model for VitalSigns

--- src/main/java/com/amaxilatis/ehr/model/VitalSigns.java
+++ src/main/java/com/amaxilatis/ehr/model/VitalSigns.java
@@ -1,20 +1,32 @@
 package com.amaxilatis.ehr.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 /**
  * @author Dimitrios Amaxilatis.
+ * @author <href="mailto:dzarras@cti.gr">Dimitris Zarras</a>
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class VitalSigns {
-    private int codingId;
-    private String unit;
+    private Long vitalSignsId = null;
+    private String codingId = "no_value";
+    private String unit = "no_value";
 
-    public int getCodingId() {
+    public Long getVitalSignsId() {
+        return vitalSignsId;
+    }
+
+    public void setVitalSignsId(final Long vitalSignsId) {
+        this.vitalSignsId = vitalSignsId;
+    }
+
+    public String getCodingId() {
         return codingId;
     }
 
-    public void setCodingId(int codingId) {
+    public void setCodingId(final String codingId) {
         this.codingId = codingId;
     }
 
@@ -22,7 +34,16 @@ public class VitalSigns {
         return unit;
     }
 
-    public void setUnit(String unit) {
+    public void setUnit(final String unit) {
         this.unit = unit;
+    }
+
+    @Override
+    public String toString() {
+        return "VitalSigns{" +
+                "vitalSignsId=" + vitalSignsId +
+                ", codingId='" + codingId + '\'' +
+                ", unit='" + unit + '\'' +
+                '}';
     }
 }
