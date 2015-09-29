84660038b9406dfdec2634d6fa91cddcc51b9a11
Dimitris Zarras <zarras.dim@gmail.com>
2015-09-29 15:07:55 +0300

    Update model for FunctionalStatus.

--- src/main/java/com/amaxilatis/ehr/model/FunctionalStatus.java
+++ src/main/java/com/amaxilatis/ehr/model/FunctionalStatus.java
@@ -1,21 +1,54 @@
 package com.amaxilatis.ehr.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 /**
  * @author Dimitrios Amaxilatis.
+ * @author <href="mailto:dzarras@cti.gr">Dimitris Zarras</a>
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class FunctionalStatus {
-    private String patientId;
-    private String status;
-    private int codingId;
+    public static FunctionalStatus empty() {
+        FunctionalStatus functionalStatus = new FunctionalStatus();
+        functionalStatus.setFunctionalStatusId(null);
+        functionalStatus.setPatientId(null);
+        functionalStatus.setStatus(null);
+        functionalStatus.setDescription(null);
+        functionalStatus.setCodingId(null);
+        functionalStatus.setCode(null);
+        functionalStatus.setCodeSystem(null);
+        return functionalStatus;
+    }
+
+    public static FunctionalStatus forPatientId(final String patientId) {
+        FunctionalStatus functionalStatus = empty();
+        functionalStatus.setPatientId(patientId);
+        return functionalStatus;
+    }
+
+    private Long functionalStatusId = null;
+    private String patientId = "no_value";
+    private String status = "no_value";
+    private String description = "no_value";
+    private String codingId = "no_value";
+    private String code = null;
+    private String codeSystem = null;
+
+    public Long getFunctionalStatusId() {
+        return functionalStatusId;
+    }
+
+    public void setFunctionalStatusId(final Long functionalStatusId) {
+        this.functionalStatusId = functionalStatusId;
+    }
 
     public String getPatientId() {
         return patientId;
     }
 
-    public void setPatientId(String patientId) {
+    public void setPatientId(final String patientId) {
         this.patientId = patientId;
     }
 
@@ -23,15 +56,52 @@ public class FunctionalStatus {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(final String status) {
         this.status = status;
     }
 
-    public int getCodingId() {
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public String getCodingId() {
         return codingId;
     }
 
-    public void setCodingId(int codingId) {
+    public void setCodingId(final String codingId) {
         this.codingId = codingId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(final String code) {
+        this.code = code;
+    }
+
+    public String getCodeSystem() {
+        return codeSystem;
+    }
+
+    public void setCodeSystem(final String codeSystem) {
+        this.codeSystem = codeSystem;
+    }
+
+    @Override
+    public String toString() {
+        return "FunctionalStatus{" +
+                "functionalStatusId=" + functionalStatusId +
+                ", patientId='" + patientId + '\'' +
+                ", status='" + status + '\'' +
+                ", description='" + description + '\'' +
+                ", codingId='" + codingId + '\'' +
+                ", code='" + code + '\'' +
+                ", codeSystem='" + codeSystem + '\'' +
+                '}';
     }
 }
